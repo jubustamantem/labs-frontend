a775416d220eddaea2d2266cbc548595db105ab2
JuanPabloBustamante <jubustamantem@unal.edu.co>
2022-05-08 16:22:33 -0500

    "Asignar rol" funcionando y botones

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container-fluid mt-2">
-    <h1>Bienvenido al dummy</h1>
+    <h1>Bienvenido. Seleccione una de las opciones a continuación:</h1>
+    <div class="row mx-auto mt-2">
+      <router-link :to="{path:'/mis-cursos'}"><button>Ver mis cursos</button></router-link>
+    </div>
+    <div class="row mx-auto mt-2">
+      <router-link :to="{path:'/principal/nuevo-rol'}"><button>Agregar rol</button></router-link>
+    </div>
     <div class="row">
       <router-view></router-view>
     </div>
