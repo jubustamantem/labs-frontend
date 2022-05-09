4a2a38b2d50aec8c540cd7c007dcdbecc44c4306
xabelian <juagarciama@unal.edu.co>
2022-05-09 15:00:02 -0500

    Added CourseForm

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Home from "./views/Home";
 import AddRole from "./components/AddRole";
 import Courses from '@/views/Courses';
 import Roles from "@/components/Roles";
+import CourseForm from "@/components/CourseForm";
 
 Vue.use(Router);
 
@@ -29,6 +30,7 @@ export default new Router({
       name: 'courses',
       component: Courses
     },
+
     {
       path: "/principal",
       name: "home",
@@ -43,7 +45,12 @@ export default new Router({
           path: "roles",
           name: "roles",
           component: Roles
-        }
+        },
+        {
+          path: '/crear-curso',
+          name: 'crear-curso',
+          component: CourseForm
+        },
       ]
     }
   ]
